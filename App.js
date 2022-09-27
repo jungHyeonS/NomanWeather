71d06f5be6ac0900df1adfaf34b3cf389a498006
jungHyeonS <fbtls25@naver.com>
2022-09-27 22:53:49 +0900

    react-native : layout system

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// component : 화면에 렌더링할 항목
+// api : 자바스크립트 코드(자바스크립트 코드가 운영체제와 소통)
+//https://reactnative.directory/
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
+    // <View style={styles.container}>
+    //   <Text style={styles.text}>Hello</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    // style={{flexDirection:"row"}}
+    <View style={{flex:1}}>
+      <View style={{flex:1, backgroundColor:"tomato"}}></View>
+      <View style={{flex:10,backgroundColor:"green"}}></View>
+      <View style={{flex:1,backgroundColor:"blue"}}></View>
     </View>
   );
 }
@@ -16,5 +25,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  text:{
+    fontSize:28
+  }
 });
