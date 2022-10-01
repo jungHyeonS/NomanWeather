88986aa232d3f6af8437b101d417bee86f7c3826
jungHyeonS <fbtls25@naver.com>
2022-10-01 11:46:42 +0900

    react-native : wehter app

--- App.js
+++ App.js
@@ -2,12 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View,ScrollView,Dimensions,ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location';
+import { Fontisto } from '@expo/vector-icons';
 const {width:SCREEN_WIDTH} = Dimensions.get("window");
 
 const API_KEY = "d847b1e9380af73454b43b1736a986bd"
 // component : 화면에 렌더링할 항목
 // api : 자바스크립트 코드(자바스크립트 코드가 운영체제와 소통)
 //https://reactnative.directory/
+
+
+const icons = {
+  "Clouds" : "cloudy",
+  "Clear" : "day-sunny",
+  "Rain" : "rain",
+  "Atmosphere":"cloudy-gusts",
+  "Snow":"snow",
+  "Drizzle":"rains",
+  "Thunderstorm":"lightning"
+}
+
 export default function App() {
   const [city,setCity] = useState("Loadding...");
   const [days,setDays] = useState([])
@@ -56,7 +69,11 @@ export default function App() {
           : (
             days.map((day,index) => 
               <View style={styles.day} key={index}>
-                <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                <View style={{flexDirection:"row", alignItems:"center",justifyContent:"space-between",width:"100%"}}>
+                  <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                  <Fontisto style={{marginTop:60}} name={icons[day.weather[0].main]} size={68} color="white" />
+                </View>
+                
                 <Text style={styles.description}>{day.weather[0].main}</Text>
                 <Text style={styles.tinyText}>{day.weather[0].description}</Text>
               </View>
@@ -79,24 +96,32 @@ const styles = StyleSheet.create({
     alignItems:"center"
   },
   cityName:{
-    fontSize:58,
-    fontWeight:"500"
+    fontSize:38,
+    fontWeight:"500",
+    color:"white"
   },  
   weather:{
   },
   day:{
     width:SCREEN_WIDTH,
-    alignItems:"center",
+    alignItems:"flex-start",
+    paddingHorizontal:20
   },
   temp:{
     marginTop:50,
-    fontSize:128
+    fontSize:100,
+    color:"white"
   },
   description:{
-    marginTop:-30,
-    fontSize:60
+    marginTop:-10,
+    fontSize:30,
+    color:"white",
+    fontWeight:"500"
   },
   tinyText:{
-    fontSize:20
+    marginTop:-5,
+    fontSize:25,
+    color:"white",
+    fontWeight:"500"
   }
 });
